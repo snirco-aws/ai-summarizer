cfa7b73eaadb79c71b038abccd2406f445397795
meidansi <meidansi@amazon.com>
2023-07-17 10:47:56 +0300

    nav-color-change

--- frontend/src/Summarizer.js
+++ frontend/src/Summarizer.js
@@ -1,39 +1,43 @@
-import React, { useState } from 'react';
-import './Summarizer.css';
-import Summarize from './Summarize';
-import JobHistory from './JobHistory';
-import { Tabs, Layout, Button } from 'antd';
-const { Header, Footer, Content } = Layout;
+import React, { useState } from 'react'
+import './Summarizer.css'
+import Summarize from './Summarize'
+import JobHistory from './JobHistory'
+import { Tabs, Layout, Button } from 'antd'
+const { Header, Footer, Content } = Layout
 
 const Summarizer = ({ user, signOut }) => {
-
-const items = [
-    { label: 'Summarize', key: '1', children: <Summarize />},
-    { label: 'Job History', key: '2', children: <JobHistory /> }
-]
+  const items = [
+    { label: 'Summarize', key: '1', children: <Summarize /> },
+    { label: 'Job History', key: '2', children: <JobHistory /> },
+  ]
 
   return (
     <div>
       <Layout>
-        <Header className='Header'>
-          <div className='logo'>
-            GenSum AI
-          </div>
+        <Header className="Header" color="blue-4">
+          <div className="logo">GenSum AI</div>
           <div>
-            { !user ? 'Login' : (
-            <Button onClick={signOut} className='Button'>
-              Logout
-            </Button>)
-            }
+            {!user ? (
+              'Login'
+            ) : (
+              <Button onClick={signOut} className="Button">
+                Logout
+              </Button>
+            )}
           </div>
         </Header>
-        <Content className='Content'>
-          <Tabs defaultActiveKey="1" className='Tabs' type='card' items={items} />
+        <Content className="Content">
+          <Tabs
+            defaultActiveKey="1"
+            className="Tabs"
+            type="card"
+            items={items}
+          />
         </Content>
         <Footer>Footer</Footer>
       </Layout>
     </div>
-  );
-};
+  )
+}
 
-export default Summarizer;
+export default Summarizer
